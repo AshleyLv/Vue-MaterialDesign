7bee057ec8b2b4059067f70b466cc45a4eb8bfd8
AshleyLicorice <ashley_lyf@icloud.com>
2017-01-24 16:51:43 +0800

    button

---
+++ src/components/mdButton.vue
@@ -0,0 +1,13 @@
+<template>
+	<button :type="type" :class="'md-btn ' + typeBtn + ' ' + typeColor" :disabled="disabled"><slot></slot></button>
+</template>
+<script type="text/javascript">
+	export default{
+		props:{
+			value
+		}
+	}
+</script>
+<style lang="less">
+	
+</style>
